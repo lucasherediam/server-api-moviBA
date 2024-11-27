6254e9b00e7ac0d44c0d84f4fb1ec6c4bd745943
lucasherediam <lherediamartinelli@gmail.com>
2024-11-27 17:58:49 -0300

    Minor changes

--- src/routes/busAgency.route.ts
+++ src/routes/busAgency.route.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 import { type PrismaClient } from '@prisma/client';
-import fs from 'fs';  // Importamos el módulo fs para trabajar con archivos
 
 const BusAgency = (prisma: PrismaClient) => {
   const router = Router();
 
-  // Endpoint para obtener todas las agencias con sus rutas
   router.get('/', async (req, res) => {
     try {
       const agencies = await prisma.busAgency.findMany({
@@ -22,18 +20,6 @@ const BusAgency = (prisma: PrismaClient) => {
             },
           },
         },
-      });
-
-      // Convertimos el objeto agencies a una cadena JSON
-      const agenciesText = JSON.stringify(agencies, null, 2);
-
-      // Guardamos el resultado en un archivo txt
-      fs.writeFile('agencies.txt', agenciesText, (err) => {
-        if (err) {
-          console.error('Error al guardar el archivo:', err);
-        } else {
-          console.log('Archivo guardado exitosamente');
-        }
       });
 
       res.json(agencies);
